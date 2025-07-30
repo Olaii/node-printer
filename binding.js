ee63384b4397555561805c7e98971919959f2d1f
Yaikhomba Ningthemcha <yaikhomba@addble.com>
2025-07-31 02:40:02 +0530

    WIP

--- binding.js
+++ binding.js
@@ -20,44 +20,90 @@ module.exports.printFile = printFile;
  */
 module.exports.getSupportedPrintFormats = addon.getSupportedPrintFormats;
 /*
- * Print raw data to a printer. This function is asynchronous and returns a Promise.
- * @param {object} options The print options.
- * @param {Buffer|string} options.data The data to print.
- * @param {string} [options.printer] The name of the printer. If not provided, the default printer is used.
- * @param {string} [options.docname="node print job"] The name of the document as it appears in the print queue.
- * @param {string} [options.type="RAW"] The data type (e.g., 'RAW', 'TEXT').
- * @param {object} [options.options={}] CUPS-specific options.
- * @returns {Promise<number>} A promise that resolves with the job ID.
+ print raw data. This function is intend to be asynchronous
+
+ parameters:
+ parameters - Object, parameters objects with the following structure:
+ data - String, mandatory, data to printer
+ printer - String, optional, name of the printer, if missing, will try to print to default printer
+ docname - String, optional, name of document showed in printer status
+ type - String, optional, only for wind32, data type, one of the RAW, TEXT
+ options - JS object with CUPS options, optional
+ success - Function, optional, callback function
+ error - Function, optional, callback function if exists any error
+
+ or
+
+ data - String, mandatory, data to printer
+ printer - String, optional, name of the printer, if missing, will try to print to default printer
+ docname - String, optional, name of document showed in printer status
+ type - String, optional, data type, one of the RAW, TEXT
+ options - JS object with CUPS options, optional
+ success - Function, optional, callback function with first argument job_id
+ error - Function, optional, callback function if exists any error
  */
-async function printDirect(options) {
-    if (typeof options !== 'object' || options === null) {
-        throw new TypeError('An options object is required.');
+ function printDirect(parameters){
+    var data = parameters
+        , printer
+        , docname
+        , type
+        , options
+        , success
+        , error;
+
+    if(arguments.length==1){
+        //TODO: check parameters type
+        //if (typeof parameters )
+        data = parameters.data;
+        printer = parameters.printer;
+        docname = parameters.docname;
+        type = parameters.type;
+        options = parameters.options||{};
+        success = parameters.success;
+        error = parameters.error;
+    }else{
+        printer = arguments[1];
+        type = arguments[2];
+        docname = arguments[3];
+        options = arguments[4];
+        success = arguments[5];
+        error = arguments[6];
     }
 
-    const {
-        data,
-        printer = addon.getDefaultPrinterName(),
-        docname = 'node print job',
-        type = 'RAW',
-        options: printerOptions = {}
-    } = options;
-
-    if (!data) {
-        throw new Error('A `data` field is required in the options object.');
+    if(!type){
+        type = "RAW";
     }
 
-    if (!printer) {
-        throw new Error('A `printer` name is required, or a default printer must be set.');
+    // Set default printer name
+    if(!printer) {
+        printer = addon.getDefaultPrinterName();
     }
 
-    return new Promise((resolve, reject) => {
-        try {
-            const jobId = addon.printDirect(data, printer, docname, type.toUpperCase(), printerOptions);
-            resolve(jobId);
-        } catch (e) {
-            reject(e);
+    type = type.toUpperCase();
+
+    if(!docname){
+        docname = "node print job";
+    }
+
+    if (!options){
+        options = {};
+    }
+
+    //TODO: check parameters type
+    if(addon.printDirect){// call C++ binding
+        try{
+            var res = addon.printDirect(data, printer, docname, type, options);
+            if(res){
+                success(res);
+            }else{
+                error(Error("Something wrong in printDirect"));
+            }
+        }catch (e){
+            error(e);
         }
-    });
+    }else{
+        error("Not supported");
+    }
 }
 
 /** Get printer info with jobs
@@ -65,39 +111,38 @@ async function printDirect(options) {
  * @return printer object info:
  *		TODO: to enum all possible attributes
  */
-function getPrinter(printerName) {
-    const targetPrinterName = printerName || addon.getDefaultPrinterName();
-    if (!targetPrinterName) {
-        return undefined;
-    }
-
-    const printer = addon.getPrinter(targetPrinterName);
-    if (printer) {
-        correctPrinterinfo(printer);
-    }
-    return printer;
-}
+ function getPrinter(printerName)
+ {
+     if(!printerName) {
+         printerName = addon.getDefaultPrinterName();
+     }
+     var printer = addon.getPrinter(printerName);
+     correctPrinterinfo(printer);
+     return printer;
+ }
 
 
 function correctPrinterinfo(printer) {
-    if (!printer || printer.status || !printer.options || !printer.options['printer-state']) {
+    if(printer.status || !printer.options || !printer.options['printer-state']){
         return;
     }
 
     var status = printer.options['printer-state'];
     // Add posix status
-    if (status == '3') {
+    if(status == '3'){
         status = 'IDLE'
-    } else if (status == '4') {
+    }
+    else if(status == '4'){
         status = 'PRINTING'
-    } else if (status == '5') {
+    }
+    else if(status == '5'){
         status = 'STOPPED'
     }
 
     // correct date type
     var k;
-    for (k in printer.options) {
-        if (/time$/.test(k) && printer.options[k] && !(printer.options[k] instanceof Date)) {
+    for(k in printer.options) {
+        if(/time$/.test(k) && printer.options[k] && !(printer.options[k] instanceof Date)) {
             printer.options[k] = new Date(printer.options[k] * 1000);
         }
     }
@@ -106,45 +151,77 @@ function correctPrinterinfo(printer) {
 }
 
 /**
- * Print a file to a printer. This function is asynchronous and returns a Promise.
- * Note: This function is only supported on POSIX platforms (macOS, Linux).
- * @param {object} options The print options.
- * @param {string} options.filename The path to the file to be printed.
- * @param {string} [options.printer] The name of the printer. If not provided, the default printer is used.
- * @param {string} [options.docname] The name of the document. Defaults to the filename.
- * @param {object} [options.options={}] CUPS-specific options.
- * @returns {Promise<number>} A promise that resolves with the job ID.
- */
-async function printFile(options) {
-    if (typeof options !== 'object' || options === null) {
-        throw new TypeError('An options object is required.');
+parameters:
+   parameters - Object, parameters objects with the following structure:
+      filename - String, mandatory, data to printer
+      docname - String, optional, name of document showed in printer status
+      printer - String, optional, mane of the printer, if missed, will try to retrieve the default printer name
+      success - Function, optional, callback function
+      error - Function, optional, callback function if exists any error
+*/
+function printFile(parameters){
+    var filename,
+        docname,
+        printer,
+        options,
+        success,
+        error;
+
+    if((arguments.length !== 1) || (typeof(parameters) !== 'object')){
+        throw new Error('must provide arguments object');
     }
 
-    if (!addon.printFile) {
-        throw new Error('`printFile` is not supported on this platform (Windows).');
+    filename = parameters.filename;
+    docname = parameters.docname;
+    printer = parameters.printer;
+    options = parameters.options || {};
+    success = parameters.success;
+    error = parameters.error;
+
+    if(!success){
+        success = function(){};
     }
 
-    const {
-        filename,
-        printer = addon.getDefaultPrinterName(),
-        docname = filename,
-        options: printerOptions = {}
-    } = options;
-
-    if (!filename) {
-        throw new Error('A `filename` is required in the options object.');
+    if(!error){
+        error = function(err){
+            throw err;
+        };
     }
 
-    if (!printer) {
-        throw new Error('A `printer` name is required, or a default printer must be set.');
+    if(!filename){
+        var err = new Error('must provide at least a filename');
+        return error(err);
     }
 
-    return new Promise((resolve, reject) => {
-        try {
-            const jobId = addon.printFile(filename, docname, printer, printerOptions);
-            resolve(jobId);
+    // try to define default printer name
+    if(!printer) {
+        printer = addon.getDefaultPrinterName();
+    }
+
+    if(!printer) {
+        return error(new Error('Printer parameter of default printer is not defined'));
+    }
+
+    // set filename if docname is missing
+    if(!docname){
+        docname = filename;
+    }
+
+    //TODO: check parameters type
+    if(addon.printFile){// call C++ binding
+        try{
+            // TODO: proper success/error callbacks from the extension
+            var res = addon.printFile(filename, docname, printer, options);
+
+            if(!isNaN(parseInt(res))) {
+                success(res);
+            } else {
+                error(Error(res));
+            }
         } catch (e) {
-            reject(e);
+            error(e);
         }
-    });
+    } else {
+        error("Not supported");
+    }
 }
